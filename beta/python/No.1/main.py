7ce94ded067846375987deaf68bfba5280554309
cazzano <cazzano.agent.47@gmail.com>
2025-01-23 12:06:59 +0500

    Important

--- beta/python/No.1/main.py
+++ beta/python/No.1/main.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app as application
+from flask import Flask
+
+server = Flask(__name__)
+
+# Expose the Dash app's server
+server.wsgi_app = application.server
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    application.run_server(debug=True)
