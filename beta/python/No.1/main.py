c6369b217056181f916237d9f53317cea2b18288
cazzano <cazzano.agent.47@gmail.com>
2025-01-23 12:31:30 +0500

    Important

--- beta/python/No.1/main.py
+++ beta/python/No.1/main.py
@@ -1,10 +1,11 @@
-from app import app as application
+from app import app  # Import your Dash app
 from flask import Flask
 
+# Create a Flask server
 server = Flask(__name__)
 
 # Expose the Dash app's server
-server.wsgi_app = application.server
+server.wsgi_app = app.server
 
 if __name__ == '__main__':
-    application.run_server(debug=True)
+    app.run_server(debug=False)  # This line is for local development only
